d27672c4f5416c62753b836624713e4bdb1dc881
john5tu4rt <john5tu4rt@gmail.com>
2016-10-13 23:50:40 +0100

    Removed immediate mode, created initial sprite class using vertex buffers

---
+++ OpenGLTutorial/Sprite.cpp
@@ -0,0 +1,46 @@
+#include "Sprite.h"
+
+
+
+Sprite::Sprite() {
+	m_vboID = 0;
+}
+
+
+Sprite::~Sprite() {
+}
+
+void Sprite::init(int x, int y, int width, int height) {
+	m_x = x;
+	m_y = y;
+	m_width = width;
+	m_height = height;
+
+	if (m_vboID == 0) {
+		glGenBuffers(1, &m_vboID);
+	}
+
+	float vertexData[12]; // 6 vertices with 2 co ords each
+
+	vertexData[0] = x + width;
+	vertexData[1] = y + height;
+
+	vertexData[2] = x;
+	vertexData[3] = y + height;
+
+	vertexData[4] = x;
+	vertexData[5] = y;
+
+	vertexData[6] = x;
+	vertexData[7] = y;
+
+	vertexData[8] = x + width;
+	vertexData[9] = y;
+
+	vertexData[10] = x + width;
+	vertexData[11] = y + height;
+
+}
+
+void Sprite::draw() {
+}
